747b46f88d2b1707cf0c593e622979cddf4b2700
Alexander <tim96@yandex.ru>
2016-08-06 20:46:25 +0400

    Add new fields to Logger entity.

--- src/Tim/DataBundle/Entity/Logger.php
+++ src/Tim/DataBundle/Entity/Logger.php
@@ -3,6 +3,7 @@
 namespace Tim\DataBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Logger
@@ -21,6 +22,81 @@ class Logger
      */
     private $id;
 
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="level", type="integer")
+     */
+    protected $level;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="level_name", type="string")
+     */
+    protected $levelName;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    protected $message;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="data", type="text", nullable=true)
+     */
+    protected $data;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ip_address", type="string", nullable=true)
+     */
+    protected $ipAddress;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="browser", type="string", nullable=true)
+     */
+    protected $browser;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="user_id", type="integer", nullable=true)
+     */
+    private $userId;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     **/
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->data = null;
+        $this->userId = null;
+        $this->browser = null;
+        $this->level = 100;
+        $this->lavelName = 'INFO';
+        $this->ipAddress = null;
+    }
+
+    public function __toString()
+    {
+        return $this->id ? (string)$this->id : '';
+    }
 
     /**
      * Get id
@@ -30,5 +106,189 @@ class Logger
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set level
+     *
+     * @param integer $level
+     * @return Logger
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return integer 
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set levelName
+     *
+     * @param string $levelName
+     * @return Logger
+     */
+    public function setLevelName($levelName)
+    {
+        $this->levelName = $levelName;
+
+        return $this;
+    }
+
+    /**
+     * Get levelName
+     *
+     * @return string 
+     */
+    public function getLevelName()
+    {
+        return $this->levelName;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Logger
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set data
+     *
+     * @param string $data
+     * @return Logger
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Get data
+     *
+     * @return string 
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Set ipAddress
+     *
+     * @param string $ipAddress
+     * @return Logger
+     */
+    public function setIpAddress($ipAddress)
+    {
+        $this->ipAddress = $ipAddress;
+
+        return $this;
+    }
+
+    /**
+     * Get ipAddress
+     *
+     * @return string 
+     */
+    public function getIpAddress()
+    {
+        return $this->ipAddress;
+    }
+
+    /**
+     * Set browser
+     *
+     * @param string $browser
+     * @return Logger
+     */
+    public function setBrowser($browser)
+    {
+        $this->browser = $browser;
+
+        return $this;
+    }
+
+    /**
+     * Get browser
+     *
+     * @return string 
+     */
+    public function getBrowser()
+    {
+        return $this->browser;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Logger
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Logger
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
     }
 }
