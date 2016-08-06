ef3b688113613df46f92722ab4a54f29264cf229
Alexander <tim96@yandex.ru>
2016-08-06 21:00:31 +0400

    Add Doctrine fixtures bundle to load data in database.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -44,6 +44,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
