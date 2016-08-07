a39915a1b323b18734af2ecf5b197c9de2e24fd3
Alexander <tim96@yandex.ru>
2016-08-07 14:14:12 +0400

    Add new entity IpAddress.

---
+++ src/Tim/DataBundle/Entity/IpAddress.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace Tim\DataBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * IpAddress
+ *
+ * @ORM\Table(name="data_ip_address")
+ * @ORM\Entity(repositoryClass="Tim\DataBundle\Repository\IpAddressRepository")
+ */
+class IpAddress
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="ip", type="string", length=255)
+     */
+    protected $ip;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    protected $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="author", type="integer", options={"default": 0})
+     */
+    protected $author;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="updated_by", type="integer", nullable=true, options={"default": null})
+     */
+    protected $updatedBy;
+
+    /**
+     * @var \DateTime
+     *
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     **/
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     **/
+    private $updatedAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->author = 0;
+        $this->updatedBy = null;
+    }
+
+    public function __toString()
+    {
+        return $this->id ? (string)($this->ip . '(' . $this->description . ')') : '';
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ip
+     *
+     * @param string $ip
+     * @return IpAddress
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    /**
+     * Get ip
+     *
+     * @return string 
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return IpAddress
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set author
+     *
+     * @param integer $author
+     * @return IpAddress
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return integer 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param integer $updatedBy
+     * @return IpAddress
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return integer 
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return IpAddress
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return IpAddress
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
