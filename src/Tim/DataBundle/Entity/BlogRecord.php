f3453292073a6f21e104d68274a23a2eebf59b71
Alexander <tim96@yandex.ru>
2016-08-14 19:17:53 +0400

    Add new entity BlogRecord.

---
+++ src/Tim/DataBundle/Entity/BlogRecord.php
@@ -0,0 +1,389 @@
+<?php
+
+namespace Tim\DataBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * BlogRecord
+ *
+ * @ORM\Table(name="data_blog_record")
+ * @ORM\Entity(repositoryClass="Tim\DataBundle\Repository\BlogRecordRepository")
+ */
+class BlogRecord
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    protected $title;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="title_slug", type="string", length=255)
+     */
+    protected $titleSlug;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    protected $description;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    protected $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image_path", type="string", length=255, nullable=true)
+     */
+    protected $imagePath;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     **/
+    protected $createdAt;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     **/
+    protected $updatedAt;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean", options={"default": false})
+     **/
+    protected $isActive = false;
+
+    /**
+     * @ORM\Column(name="is_publish_on_main_page", type="boolean", options={"default": false})
+     **/
+    protected $isPublishOnMainPage = false;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="author", type="integer", options={"default": 0})
+     */
+    protected $author;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="updated_by", type="integer", nullable=true, options={"default": null})
+     */
+    protected $updatedBy;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+        $this->isActive = false;
+        $this->isPublishOnMainPage = false;
+        $this->author = 0;
+        $this->updatedBy = null;
+    }
+
+    public function __toString()
+    {
+        return $this->id ? (string)$this->id : '';
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return BlogRecord
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set titleSlug
+     *
+     * @param string $titleSlug
+     * @return BlogRecord
+     */
+    public function setTitleSlug($titleSlug)
+    {
+        $this->titleSlug = $titleSlug;
+
+        return $this;
+    }
+
+    /**
+     * Get titleSlug
+     *
+     * @return string 
+     */
+    public function getTitleSlug()
+    {
+        return $this->titleSlug;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return BlogRecord
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return BlogRecord
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set imagePath
+     *
+     * @param string $imagePath
+     * @return BlogRecord
+     */
+    public function setImagePath($imagePath)
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+
+    /**
+     * Get imagePath
+     *
+     * @return string 
+     */
+    public function getImagePath()
+    {
+        return $this->imagePath;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return BlogRecord
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return BlogRecord
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return BlogRecord
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Set isPublishOnMainPage
+     *
+     * @param boolean $isPublishOnMainPage
+     * @return BlogRecord
+     */
+    public function setIsPublishOnMainPage($isPublishOnMainPage)
+    {
+        $this->isPublishOnMainPage = $isPublishOnMainPage;
+
+        return $this;
+    }
+
+    /**
+     * Get isPublishOnMainPage
+     *
+     * @return boolean 
+     */
+    public function getIsPublishOnMainPage()
+    {
+        return $this->isPublishOnMainPage;
+    }
+
+    /**
+     * Set author
+     *
+     * @param integer $author
+     * @return BlogRecord
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return integer 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param integer $updatedBy
+     * @return BlogRecord
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return integer 
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+}
