c5bee21f31c2010933806e05055ad75dcfba4b3b
Alexander <tim96@yandex.ru>
2016-08-15 21:27:12 +0400

    Add 'AboutItemUploadListener' to upload file for entity 'AboutItem'.

---
+++ src/Tim/DataBundle/EventListener/AboutItemUploadListener.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tim\DataBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Tim\DataBundle\Entity\AboutItem;
+use Tim\DataBundle\Service\FileUploaderService;
+
+class AboutItemUploadListener
+{
+    private $uploader;
+
+    public function __construct(FileUploaderService $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    private function uploadFile($entity)
+    {
+        if (!$entity instanceof AboutItem) {
+            return;
+        }
+
+        $file = $entity->getImagePath();
+
+        // only upload new files
+        if (!$file instanceof UploadedFile) {
+            return;
+        }
+
+        $fileName = $this->uploader->upload($file);
+        $entity->setImagePath($fileName);
+    }
+}
