30ea2061701fb285bee7df83ab956eb05d0d83d1
Alexander <tim96@yandex.ru>
2016-08-17 22:01:21 +0400

    Add admin screen for About entity.

---
+++ src/Tim/BackendBundle/Admin/AboutAdmin.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Tim\BackendBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Tim\DataBundle\Entity\About;
+
+class AboutAdmin extends AbstractAdmin
+{
+    public function __construct($code, $class, $baseControllerName)
+    {
+        parent::__construct($code, $class, $baseControllerName);
+
+        $this->datagridValues = array(
+            '_sort_order' => 'DESC',
+            '_sort_by' => 'id',
+        );
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     * @throws \RuntimeException
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('author')
+            ->add('updatedBy')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     * @throws \RuntimeException
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+            ->add('id')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('author')
+            ->add('updatedBy')
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            // ->add('id')
+            ->add('description')
+            ->add('blogRecord', null, array('property' => 'getTitleForSelect'))
+            ->add('aboutItems', null, array('multiple' => true, 'property' => 'getTitleForSelect'))
+            // ->add('createdAt')
+            // ->add('updatedAt')
+            // ->add('author')
+            // ->add('updatedBy')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     * @throws \RuntimeException
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('description')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('author')
+            ->add('updatedBy')
+        ;
+    }
+
+    public function prePersist($object)
+    {
+        parent::prePersist($object);
+
+        $this->setAuthor($object);
+    }
+
+    public function preUpdate($object)
+    {
+        parent::preUpdate($object);
+
+        $this->setUpdatedBy($object);
+    }
+
+    /**
+     * @param About $object
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
+     */
+    protected function setAuthor($object)
+    {
+        $container = $this->getConfigurationPool()->getContainer();
+        $user = $container->get('security.token_storage')->getToken()->getUser();
+
+        $object->setAuthor($user->getId());
+        $object->setCreatedAt(new \DateTime());
+    }
+
+    /**
+     * @param About $object
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException
+     * @throws \Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
+     */
+    protected function setUpdatedBy($object)
+    {
+        $container = $this->getConfigurationPool()->getContainer();
+        $user = $container->get('security.token_storage')->getToken()->getUser();
+
+        $object->setUpdatedBy($user->getId());
+        $object->setUpdatedAt(new \DateTime());
+    }
+}
---
+++ src/Tim/BackendBundle/Controller/AboutAdminController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tim\BackendBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class AboutAdminController extends CRUDController
+{
+
+}
