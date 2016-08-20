983f4c4e2cd55dafbffd055d32100a2a90edff5d
Alexander <tim96@yandex.ru>
2016-08-20 22:49:06 +0400

    Add testimonial block to index layout.

--- src/Tim/FrontendBundle/Controller/FrontendController.php
+++ src/Tim/FrontendBundle/Controller/FrontendController.php
@@ -17,17 +17,23 @@ class FrontendController extends Controller
         $sliderInfoService = $this->container->get('tim_data.slider_info_handler');
         $aboutService = $this->container->get('tim_data.about_handler');
         $chefService = $this->container->get('tim_data.chef_handler');
+        $testimonialService = $this->container->get('tim_data.testimonial_handler');
+
         $uploadService = $this->container->get('tim_data.about_item_upload_listener');
         $uploadChefImageService = $this->container->get('tim_data.chef_upload_listener');
+        $uploadTestimonialImageService = $this->container->get('tim_data.testimonial_upload_listener');
 
         $sliderInfo = $sliderInfoService->getSliderInfo()->getArrayResult();
+
         $aboutInfo = $aboutService->getAboutBlocks($uploadService);
         $chefs = $chefService->getChefs($uploadChefImageService);
+        $testimonials = $testimonialService->getTestimonials($uploadTestimonialImageService);
 
         return $this->render('TimFrontendBundle:Frontend:index.html.twig', array(
             'sliderInfo' => $sliderInfo,
             'aboutInfo' => $aboutInfo,
             'chefs' => $chefs,
+            'testimonials' => $testimonials,
         ));
     }
 }
