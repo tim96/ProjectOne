ec03087b8368550f0c0a7a0bd610d45419d0e146
Alexander <tim96@yandex.ru>
2016-08-20 22:09:33 +0400

    Add chef block to main layout.

--- src/Tim/FrontendBundle/Controller/FrontendController.php
+++ src/Tim/FrontendBundle/Controller/FrontendController.php
@@ -16,14 +16,18 @@ class FrontendController extends Controller
     {
         $sliderInfoService = $this->container->get('tim_data.slider_info_handler');
         $aboutService = $this->container->get('tim_data.about_handler');
+        $chefService = $this->container->get('tim_data.chef_handler');
         $uploadService = $this->container->get('tim_data.about_item_upload_listener');
+        $uploadChefImageService = $this->container->get('tim_data.chef_upload_listener');
 
         $sliderInfo = $sliderInfoService->getSliderInfo()->getArrayResult();
         $aboutInfo = $aboutService->getAboutBlocks($uploadService);
+        $chefs = $chefService->getChefs($uploadChefImageService);
 
         return $this->render('TimFrontendBundle:Frontend:index.html.twig', array(
             'sliderInfo' => $sliderInfo,
             'aboutInfo' => $aboutInfo,
+            'chefs' => $chefs,
         ));
     }
 }
