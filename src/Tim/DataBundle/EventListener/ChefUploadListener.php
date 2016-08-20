2d15745cefef7b7d91157b0895910e9aa4710dbf
Alexander <tim96@yandex.ru>
2016-08-20 19:42:41 +0400

    Add ChefUploadListener.

---
+++ src/Tim/DataBundle/EventListener/ChefUploadListener.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tim\DataBundle\EventListener;
+
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Tim\DataBundle\Entity\Chef;
+
+class ChefUploadListener extends UploadListener
+{
+    protected function uploadFile($entity)
+    {
+        if (!$entity instanceof Chef) {
+            return;
+        }
+
+        $file = $entity->getFile();
+
+        // only upload new files
+        if (!$file instanceof UploadedFile) {
+            return;
+        }
+
+        $previousFilePath = $entity->getImagePath();
+        if (null !== $previousFilePath) {
+            $filePath = $file->getPathname();
+            // if the same file don't update data
+            if ($this->uploader->getHash($filePath) === $this->uploader->getFileHash($previousFilePath)) {
+                return ;
+            }
+        }
+
+        $fileName = $this->uploader->upload($file);
+        $entity->setImagePath($fileName);
+
+        if (null !== $previousFilePath) {
+            $this->uploader->remove($previousFilePath);
+        }
+    }
+
+    public function postLoad(LifecycleEventArgs $args)
+    {
+        /** @var Chef $entity */
+        $entity = $args->getObject();
+
+        $fileName = $entity->getImagePath();
+
+        $entity->setFile(new File($this->uploader->getTargetDir() . DIRECTORY_SEPARATOR . $fileName));
+    }
+}
