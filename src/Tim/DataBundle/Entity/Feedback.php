3c95e8cc2fb96b6de473d126afc7b4cf7fe7e59d
Alexander <tim96@yandex.ru>
2016-08-21 13:30:59 +0400

    Add Feedback entity.

---
+++ src/Tim/DataBundle/Entity/Feedback.php
@@ -0,0 +1,354 @@
+<?php
+
+namespace Tim\DataBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Feedback
+ *
+ * @ORM\Table(name="data_feedback")
+ * @ORM\Entity(repositoryClass="Tim\DataBundle\Repository\FeedbackRepository")
+ */
+class Feedback
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    protected $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name_customer", type="string", length=255, nullable=true)
+     */
+    protected $nameCustomer;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phone_customer", type="string", length=255, nullable=true)
+     */
+    protected $phoneCustomer;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank()
+     * @Assert\NotNull()
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    protected $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image_path", type="string", length=255, nullable=true)
+     */
+    protected $imagePath;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     **/
+    protected $createdAt;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     **/
+    protected $updatedAt;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean", options={"default": false})
+     **/
+    protected $isActive = false;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="author", type="integer", options={"default": 0})
+     */
+    protected $author;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="updated_by", type="integer", nullable=true, options={"default": null})
+     */
+    protected $updatedBy;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+        $this->isActive = false;
+        $this->author = 0;
+        $this->updatedBy = null;
+    }
+
+    public function __toString()
+    {
+        return $this->id ? (string)$this->id : '';
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Feedback
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set nameCustomer
+     *
+     * @param string $nameCustomer
+     * @return Feedback
+     */
+    public function setNameCustomer($nameCustomer)
+    {
+        $this->nameCustomer = $nameCustomer;
+
+        return $this;
+    }
+
+    /**
+     * Get nameCustomer
+     *
+     * @return string 
+     */
+    public function getNameCustomer()
+    {
+        return $this->nameCustomer;
+    }
+
+    /**
+     * Set phoneCustomer
+     *
+     * @param string $phoneCustomer
+     * @return Feedback
+     */
+    public function setPhoneCustomer($phoneCustomer)
+    {
+        $this->phoneCustomer = $phoneCustomer;
+
+        return $this;
+    }
+
+    /**
+     * Get phoneCustomer
+     *
+     * @return string 
+     */
+    public function getPhoneCustomer()
+    {
+        return $this->phoneCustomer;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Feedback
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set imagePath
+     *
+     * @param string $imagePath
+     * @return Feedback
+     */
+    public function setImagePath($imagePath)
+    {
+        $this->imagePath = $imagePath;
+
+        return $this;
+    }
+
+    /**
+     * Get imagePath
+     *
+     * @return string 
+     */
+    public function getImagePath()
+    {
+        return $this->imagePath;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Feedback
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Feedback
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return Feedback
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Set author
+     *
+     * @param integer $author
+     * @return Feedback
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return integer 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param integer $updatedBy
+     * @return Feedback
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return integer 
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+}
